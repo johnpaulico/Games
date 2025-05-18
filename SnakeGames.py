ae2a337709a0aa9402f59cf58d7e05a8fcce3108
johnpaulico <138341003+johnpaulico@users.noreply.github.com>
2025-05-19 00:14:46 +0800

    Update SnakeGames.py

--- SnakeGames.py
+++ SnakeGames.py
@@ -1,5 +1,5 @@
-from random import randrange
-from turtle import *
+from random import randrange 
+from turtle import * 
 
 from freegames import square, vector
 
@@ -16,13 +16,13 @@ def change(x, y):
 
 def inside(head):
     """Return True if head inside boundaries."""
-    return -200 < head.x < 190 and -200 < head.y < 190
+    return -200 < head.x < 190 and -200 < head.y < 190 
 
 
 def move():
     """Move snake forward one segment."""
     head = snake[-1].copy()
-    head.move(aim)
+    head.move(aim) 
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
